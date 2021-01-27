c57e689b324fa9bbd46a3a0685d34635c7899b59
蒋思凡 <jiangsf8@midea.com>
2021-01-27 09:17:18 +0000

    Update

---
+++ online_inference.py
@@ -0,0 +1,42 @@
+import time
+
+import cv2
+import torch
+
+from metrics import AverageMeter, Result
+
+if __name__ == '__main__':
+    alpha = 0.3
+    output_directory = 'results/'
+    checkpoint = torch.load(
+        'results/mobilenet-nnconv5dw-skipadd-pruned.pth.tar')
+    if isinstance(checkpoint, dict):
+        start_epoch = checkpoint['epoch']
+        best_result = checkpoint['best_result']
+        model = checkpoint['model']
+    else:
+        start_epoch = 0
+        model = checkpoint
+    model.eval()
+
+    end = time.time()
+    capture = cv2.VideoCapture(0)
+    while capture.isOpened():
+        ret, bgr = capture.read()
+        rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+        rgb = rgb.cuda()
+        data_time = time.time() - end
+
+        end = time.time()
+        with torch.no_grad():
+            pred = model(rgb)
+        gpu_time = time.time() - end
+
+        heat = cv2.applyColorMap(pred.data, cv2.COLORMAP_JET)
+        bgr = cv2.addWeighted(heat, alpha, bgr, 1-alpha, 0)
+
+        cv2.imwrite('TEST.png', bgr)        
+        key = cv2.waitKey(0)
+        if key == ord('q'):
+            break
+
