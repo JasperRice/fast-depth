71f32bb7385be58b1b667095e886761c25a0b0c3
蒋思凡 <jiangsf8@midea.com>
2021-01-28 17:21:12 +0800

    Add preprocess of the image

--- online_inference.py
+++ online_inference.py
@@ -1,14 +1,24 @@
 import time
 
 import cv2
+import numpy as np
 import torch
+
+
+def preprocess(frame):
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb = np.asfarray(rgb, dtype=np.float) / 255
+    rgb = np.expand_dims(rgb, axis=0)
+    rgb = np.transpose(rgb, axes=[0, 3, 1, 2])
+    rgb = torch.from_numpy(rgb).float().cuda()
+    return rgb
 
 
 if __name__ == '__main__':
     alpha = 0.3
     output_directory = 'weights/'
-    checkpoint = torch.load(
-        'weights/mobilenet-nnconv5dw-skipadd-pruned.pth.tar')
+    model_file = 'weights/mobilenet-nnconv5dw-skipadd-pruned.pth.tar'
+    checkpoint = torch.load(model_file)
     if isinstance(checkpoint, dict):
         start_epoch = checkpoint['epoch']
         best_result = checkpoint['best_result']
@@ -22,8 +32,7 @@ if __name__ == '__main__':
     capture = cv2.VideoCapture(0)
     while capture.isOpened():
         ret, bgr = capture.read()
-        rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
-        rgb = torch.from_numpy(rgb)
+        rgb = preprocess(bgr)
         data_time = time.time() - end
 
         end = time.time()
